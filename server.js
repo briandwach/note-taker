c860f8ee984e7633f3292cd882482b0d96018bcf
livefromthelevel <brian.d.wach@gmail.com>
2024-03-03 13:18:50 -0600

    <Returned default route to a wildcard route>

--- server.js
+++ server.js
@@ -17,14 +17,14 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-// Default and wildcard route to direct user to homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
-
 // GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+// Default and wildcard route to direct user to homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 app.listen(PORT, () =>
