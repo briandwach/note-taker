6098d003e1838e2719adf50b9df4b95fa7b26fae
livefromthelevel <brian.d.wach@gmail.com>
2024-03-03 10:56:39 -0600

    <Notes api routes are functioning for get, post, and delete>

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { clog } = require('./middleware/clog');
+const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,16 +10,21 @@ const app = express();
 // Import middleware, "cLog"
 app.use(clog);
 
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
 app.use(express.static('public'));
+
+// Default and wildcard route to direct user to homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
-
-// Default and wildcard route to direct user to homepage
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 app.listen(PORT, () =>
